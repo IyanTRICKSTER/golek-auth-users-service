46e14f0f70812a0c7d707c9f981b1d467fad8c6a
iyan <akiyan2002@gmail.com>
2022-07-15 00:41:20 +0700

    Fixing cors

--- pkg/models/permission.go
+++ pkg/models/permission.go
@@ -7,8 +7,13 @@ import (
 type Permission struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
 	Name      string     `json:"name"`
+	Code      string     `json:"code"`
 	Roles     []Role     `gorm:"many2many:role_permissions;" json:"roles,omitempty"`
 	CreatedAt time.Time  `json:"-"`
 	UpdatedAt time.Time  `json:"-"`
 	DeletedAt *time.Time `sql:"index" json:"-"`
+}
+
+func (p *Permission) ConstructPermissionCode() {
+
 }
